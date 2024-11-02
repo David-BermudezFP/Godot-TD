149b5f7f6e2e6881d8a494f465412ada3b58be3e
David-BermudezFP <dbermudezl24@fpcoruna.afundacion.org>
2024-11-02 10:49:32 +0100

    feature: Create character and animations

---
+++ animation_player.gd
@@ -0,0 +1,20 @@
+extends AnimationPlayer
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if Input.is_action_pressed("ui_right"):
+		play("derecha")
+	elif Input.is_action_pressed("ui_left"):
+		play("Izquierda")
+	elif Input.is_action_pressed("ui_down"):
+		play("Abajo")
+	elif Input.is_action_pressed("ui_up"):
+		play("Arriba")
+	else:
+		play("RESET")
---
+++ character_body_2d.gd
@@ -0,0 +1,24 @@
+extends CharacterBody2D
+
+
+# Declaración de la velocidad y la constante de velocidad.
+@export var SPEED := 190 # Ajusta la velocidad según lo necesites.
+const JUMP_VELOCITY = -700.0
+
+
+func _physics_process(delta: float) -> void:
+	# Añadir direcciones de movimiento
+	var direction_x := Input.get_axis("ui_left", "ui_right")
+	var direction_y := Input.get_axis("ui_up", "ui_down")
+
+	# Establece la velocidad en `x` e `y` según la dirección y la velocidad.
+	self.velocity.x = direction_x * SPEED
+	self.velocity.y = direction_y * SPEED
+
+	# Mueve al personaje según la velocidad calculada.
+	move_and_slide()
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	
+	pass # Replace with function body.
