f830ff5f9ec551d1d4a6bfe84cf550f9fd275d0b
David-BermudezFP <dbermudezl24@fpcoruna.afundacion.org>
2024-11-04 19:12:27 +0100

    Chore: Create bullet scene hoja for torre_1 and changed the way enemies spawn

--- Scripts/enemigo_agua_1.gd
+++ Scripts/enemigo_agua_1.gd
@@ -2,6 +2,7 @@ extends CharacterBody2D
 
 
 @export var speed = 50
+var health = 2
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -9,3 +10,5 @@ func _process(delta):
 	if get_parent().get_progress_ratio() == 1:
 		queue_free()
 	
+	if health <= 0:
+		get_parent().get_parent().queue_free()
--- Scripts/torre_1.gd
+++ Scripts/torre_1.gd
@@ -1,15 +1,36 @@
 extends Node2D
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+var Bullet = preload("res://Escenas/hoja.tscn")
+var bulletDamage = 1
+var pathName
+var currTargets = []
+var curr
 
 
-func _on_area_2d_area_entered(area: Area2D) -> void:
-	pass # Replace with function body.
+
+func _on_tower_body_shape_entered(body_rid: RID, body: Node2D, body_shape_index: int, local_shape_index: int) -> void:
+	if body.name.contains("Enemigo"):
+		var tempArray = []
+		currTargets = get_node("Tower").get_overlapping_bodies()
+		
+		for i in currTargets:
+			if "Enemigo" in i.name:
+				tempArray.append(i)
+				
+		var currTarget = null
+		
+		for i in tempArray:
+			if currTarget == null:
+				currTarget = i.get_node("../")
+			else:
+				if i.get_parent().get_progress() > currTarget.get_progress():
+					currTarget = i.get_node("../")
+		
+		curr = currTarget
+		pathName = currTarget.get_parent().name
+		
+		var tempBullet = Bullet.instantiate()
+		tempBullet.pathName = pathName
+		tempBullet.bulletDamage = bulletDamage
+		get_node("BulletContainer").add_child(tempBullet)
+		tempBullet.global_position = $Aim.global_position
